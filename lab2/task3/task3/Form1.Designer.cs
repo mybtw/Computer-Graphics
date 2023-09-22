39eeda33715fb8c125faa16f1fb5b5e322d5dafc
mybtw <ilyabigk@gmail.com>
2023-09-22 19:12:18 +0300

    Add task 3 RGB to HSV

---
+++ lab2/task3/task3/Form1.Designer.cs
@@ -0,0 +1,305 @@
+﻿namespace task3
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.sourceImageBox = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.numericUpDown1 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown2 = new System.Windows.Forms.NumericUpDown();
+            this.numericUpDown3 = new System.Windows.Forms.NumericUpDown();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.button2 = new System.Windows.Forms.Button();
+            this.pictureBoxHSV = new System.Windows.Forms.PictureBox();
+            this.label7 = new System.Windows.Forms.Label();
+            this.label8 = new System.Windows.Forms.Label();
+            this.button3 = new System.Windows.Forms.Button();
+            this.button4 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.sourceImageBox)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxHSV)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11F);
+            this.button1.Location = new System.Drawing.Point(141, 424);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(144, 56);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "Выбрать картинку";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // sourceImageBox
+            // 
+            this.sourceImageBox.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.sourceImageBox.Location = new System.Drawing.Point(12, 12);
+            this.sourceImageBox.Name = "sourceImageBox";
+            this.sourceImageBox.Size = new System.Drawing.Size(481, 406);
+            this.sourceImageBox.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.sourceImageBox.TabIndex = 1;
+            this.sourceImageBox.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label1.FlatStyle = System.Windows.Forms.FlatStyle.Flat;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label1.Location = new System.Drawing.Point(147, 498);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(32, 31);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "H";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label2.Location = new System.Drawing.Point(147, 551);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(31, 31);
+            this.label2.TabIndex = 6;
+            this.label2.Text = "S";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.label3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label3.Location = new System.Drawing.Point(147, 607);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(30, 31);
+            this.label3.TabIndex = 7;
+            this.label3.Text = "V";
+            // 
+            // numericUpDown1
+            // 
+            this.numericUpDown1.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.numericUpDown1.Location = new System.Drawing.Point(210, 498);
+            this.numericUpDown1.Maximum = new decimal(new int[] {
+            360,
+            0,
+            0,
+            0});
+            this.numericUpDown1.Minimum = new decimal(new int[] {
+            360,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown1.Name = "numericUpDown1";
+            this.numericUpDown1.Size = new System.Drawing.Size(75, 34);
+            this.numericUpDown1.TabIndex = 8;
+            this.numericUpDown1.ValueChanged += new System.EventHandler(this.numericUpDown1_ValueChanged);
+            // 
+            // numericUpDown2
+            // 
+            this.numericUpDown2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.numericUpDown2.Location = new System.Drawing.Point(210, 604);
+            this.numericUpDown2.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown2.Name = "numericUpDown2";
+            this.numericUpDown2.Size = new System.Drawing.Size(75, 34);
+            this.numericUpDown2.TabIndex = 9;
+            this.numericUpDown2.ValueChanged += new System.EventHandler(this.numericUpDown2_ValueChanged);
+            // 
+            // numericUpDown3
+            // 
+            this.numericUpDown3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.numericUpDown3.Location = new System.Drawing.Point(210, 551);
+            this.numericUpDown3.Minimum = new decimal(new int[] {
+            100,
+            0,
+            0,
+            -2147483648});
+            this.numericUpDown3.Name = "numericUpDown3";
+            this.numericUpDown3.Size = new System.Drawing.Size(75, 34);
+            this.numericUpDown3.TabIndex = 10;
+            this.numericUpDown3.ValueChanged += new System.EventHandler(this.numericUpDown3_ValueChanged);
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label4.Location = new System.Drawing.Point(291, 500);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(21, 29);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "°";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label5.Location = new System.Drawing.Point(291, 553);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(35, 29);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "%";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label6.Location = new System.Drawing.Point(291, 607);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(35, 29);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "%";
+            // 
+            // button2
+            // 
+            this.button2.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.button2.Location = new System.Drawing.Point(327, 424);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(166, 56);
+            this.button2.TabIndex = 14;
+            this.button2.Text = "TO HSV";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // pictureBoxHSV
+            // 
+            this.pictureBoxHSV.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBoxHSV.Location = new System.Drawing.Point(816, 12);
+            this.pictureBoxHSV.Name = "pictureBoxHSV";
+            this.pictureBoxHSV.Size = new System.Drawing.Size(475, 406);
+            this.pictureBoxHSV.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBoxHSV.TabIndex = 15;
+            this.pictureBoxHSV.TabStop = false;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label7.Location = new System.Drawing.Point(499, 25);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(64, 29);
+            this.label7.TabIndex = 16;
+            this.label7.Text = "RGB";
+            // 
+            // label8
+            // 
+            this.label8.AutoSize = true;
+            this.label8.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.label8.Location = new System.Drawing.Point(749, 25);
+            this.label8.Name = "label8";
+            this.label8.Size = new System.Drawing.Size(61, 29);
+            this.label8.TabIndex = 17;
+            this.label8.Text = "HSV";
+            // 
+            // button3
+            // 
+            this.button3.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.button3.Location = new System.Drawing.Point(911, 424);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(159, 46);
+            this.button3.TabIndex = 18;
+            this.button3.Text = "Save";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click);
+            // 
+            // button4
+            // 
+            this.button4.Font = new System.Drawing.Font("Microsoft Sans Serif", 14F);
+            this.button4.Location = new System.Drawing.Point(327, 537);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(166, 56);
+            this.button4.TabIndex = 19;
+            this.button4.Text = "Apply";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(1393, 649);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.label8);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.pictureBoxHSV);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.numericUpDown3);
+            this.Controls.Add(this.numericUpDown2);
+            this.Controls.Add(this.numericUpDown1);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.sourceImageBox);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            ((System.ComponentModel.ISupportInitialize)(this.sourceImageBox)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown1)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown2)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.numericUpDown3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBoxHSV)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox sourceImageBox;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.NumericUpDown numericUpDown1;
+        private System.Windows.Forms.NumericUpDown numericUpDown2;
+        private System.Windows.Forms.NumericUpDown numericUpDown3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBoxHSV;
+        private System.Windows.Forms.Label label7;
+        private System.Windows.Forms.Label label8;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Button button4;
+    }
+}
+
---
+++ lab2/task3/task3/Form1.cs
@@ -0,0 +1,232 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace task3
+{
+    public partial class Form1 : Form
+    {
+        private int H = 360;
+        private double S = 1;
+        private double L = 1;
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            OpenFileDialog openFileDialog = new OpenFileDialog
+            {
+                Filter = "Image Files(*.BMP;*.JPG;*.GIF;*.PNG)|*.BMP;*.JPG;*.GIF;*.PNG"
+            };
+
+            if (openFileDialog.ShowDialog() == DialogResult.OK)
+            {
+                try
+                {
+                    sourceImageBox.Image = new Bitmap(openFileDialog.FileName);
+                }
+                catch
+                {
+                    MessageBox.Show("Unable to open the selected file", "Error", MessageBoxButtons.OK, MessageBoxIcon.Error);
+                }
+            }
+        }
+
+        public static void ColorToHSV(Color color, out double hue, out double saturation, out double value)
+        {
+            double normG = color.G / 255.0;
+            double normR = color.R / 255.0;
+            double normB = color.B / 255.0;
+
+            double max = Math.Max(normR, Math.Max(normG, normB));
+            double min = Math.Min(normR, Math.Min(normG, normB));
+
+            double diff = max - min;
+
+            if (max == min)
+            {
+                hue = 0;
+            }
+            else if (max == normR && color.G >= color.B)
+            {
+                hue = 60 * ((normG - normB) / (diff));
+            }
+            else if (max == normR && color.G < color.B)
+            {
+                hue = 60 * (normG - normB) / (diff) + 360;
+            }
+            else if (max == normG)
+            {
+                hue = 60 * ((normB - normR) / (diff)) + 120;
+            }
+            else
+            {
+                hue = 60 * (normR - normG) / diff + 240;
+            }
+            saturation = max == 0 ? 0 : 1 - (min / max);
+            value = max;
+        }
+
+        public static Color ColorFromHSV(double hue, double saturation, double value)
+        {
+
+            double hi = Convert.ToInt32(Math.Floor(hue / 60.0)) % 6;
+            double vmin = Convert.ToInt32(((100 - saturation) * value) / 100);
+            double a = Convert.ToInt32((value - vmin) * (hue % 60) / 60);
+            double vinc = Convert.ToInt32(vmin + a);
+            double vdec = Convert.ToInt32(value - a);
+
+            int v = Convert.ToInt32(value * 255 / 100);
+            int p_vinc = Convert.ToInt32(vinc * 255 / 100);
+            int p_vmin = Convert.ToInt32(vmin * 255 / 100);
+            int p_vdec = Convert.ToInt32(vdec * 255 / 100);
+
+            if (hi == 0)
+            {
+                return Color.FromArgb(255, v, p_vinc, p_vmin);
+            }
+            else if (hi == 1)
+            {
+                return Color.FromArgb(255, p_vdec, v, p_vmin);
+            }
+            else if (hi == 2)
+            {
+                return Color.FromArgb(255, p_vmin, v, p_vinc);
+            }
+            else if (hi == 3)
+            {
+                return Color.FromArgb(255, p_vmin, p_vdec, v);
+            }
+            else if (hi == 4)
+            {
+                return Color.FromArgb(255, p_vinc, p_vmin, v);
+            }
+            else
+            {
+                return Color.FromArgb(255, v, p_vmin, p_vdec);
+            }
+        }
+
+
+        private void numericUpDown1_ValueChanged(object sender, EventArgs e)
+        {
+            this.H = (int)numericUpDown1.Value;
+        }
+
+        private void numericUpDown3_ValueChanged(object sender, EventArgs e)
+        {
+            this.S = (int)numericUpDown3.Value / 100;
+        }
+
+        private void numericUpDown2_ValueChanged(object sender, EventArgs e)
+        {
+            this.L = (int)numericUpDown2.Value / 100;
+        }
+
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Image rgbImage = pictureBoxHSV.Image;
+
+            using (SaveFileDialog saveDialog = new SaveFileDialog())
+            {
+                saveDialog.Filter = "JPEG Image|*.jpg";
+
+                if (saveDialog.ShowDialog() == DialogResult.OK)
+                {
+                    string savePath = saveDialog.FileName;
+                    // Сохраните изображение в RGB формате
+                    rgbImage.Save(savePath, System.Drawing.Imaging.ImageFormat.Jpeg);
+                }
+            }
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+            Bitmap originalImage = new Bitmap(sourceImageBox.Image);
+
+            Bitmap hsvImage = new Bitmap(originalImage.Width, originalImage.Height);
+
+            for (int x = 0; x < originalImage.Width; x++)
+            {
+                for (int y = 0; y < originalImage.Height; y++)
+                {
+                    Color rgbColor = originalImage.GetPixel(x, y);
+
+                    double hue, saturation, value;
+                    ColorToHSV(rgbColor, out hue, out saturation, out value);
+
+                    Color hsvColor = ColorFromHSV(hue, saturation * 100, value * 100);
+
+                    hsvImage.SetPixel(x, y, hsvColor);
+                }
+            }
+
+            pictureBoxHSV.Image = hsvImage;
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            int v = (int)numericUpDown2.Value;
+            int s = (int)numericUpDown3.Value;
+            int h = (int)numericUpDown1.Value;
+
+            Bitmap originalImage = new Bitmap(sourceImageBox.Image);
+
+            Bitmap hsvImage = new Bitmap(originalImage.Width, originalImage.Height);
+
+            for (int x = 0; x < originalImage.Width; x++)
+            {
+                for (int y = 0; y < originalImage.Height; y++)
+                {
+                    Color rgbColor = originalImage.GetPixel(x, y);
+
+                    double hue, saturation, value;
+                    ColorToHSV(rgbColor, out hue, out saturation, out value);
+
+                    hue += (360 / 100) * h / 100;
+                    if (hue < 0) 
+                    {
+                        hue = 0;
+                    }
+                    if (hue > 360)
+                    {
+                        hue = 360;
+                    }
+                    saturation += 0.01 * s;
+                    if (saturation < 0)
+                    {
+                        saturation = 0;
+                    }
+                    if (saturation > 1)
+                    {
+                       saturation = 1;
+                    }
+                    value += 0.01 * v;
+                    if (value < 0)
+                    {
+                        value = 0;
+                    }
+                    if (value > 1)
+                    {
+                        value = 1;
+                    }
+
+                    Color hsvColor = ColorFromHSV(hue, saturation * 100, value * 100);
+
+                    hsvImage.SetPixel(x, y, hsvColor);
+                }
+            }
+
+            pictureBoxHSV.Image = hsvImage;
+        }
+    }
+}
