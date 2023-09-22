ddef062d7982f55901d399adc79074d3ed0994b3
Moirted <moirted@gmail.com>
2023-09-22 20:52:37 +0300

    Lab 1 task 1 implemented

--- lab2/task1/lesson2_task1/lesson2_task1/Form1.Designer.cs
+++ lab2/task1/lesson2_task1/lesson2_task1/Form1.Designer.cs
@@ -35,16 +35,28 @@
             this.pictureBox2 = new System.Windows.Forms.PictureBox();
             this.pictureBox3 = new System.Windows.Forms.PictureBox();
             this.button2 = new System.Windows.Forms.Button();
+            this.pictureBox4 = new System.Windows.Forms.PictureBox();
+            this.pictureBox5 = new System.Windows.Forms.PictureBox();
+            this.pictureBox6 = new System.Windows.Forms.PictureBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).BeginInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).BeginInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).BeginInit();
             this.SuspendLayout();
             // 
             // button1
             // 
-            this.button1.Location = new System.Drawing.Point(42, 161);
+            this.button1.Location = new System.Drawing.Point(58, 230);
+            this.button1.Margin = new System.Windows.Forms.Padding(4);
             this.button1.Name = "button1";
-            this.button1.Size = new System.Drawing.Size(103, 39);
+            this.button1.Size = new System.Drawing.Size(137, 48);
             this.button1.TabIndex = 0;
             this.button1.Text = "Загрузить";
             this.button1.UseVisualStyleBackColor = true;
@@ -53,9 +65,10 @@
             // pictureBox1
             // 
             this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox1.Location = new System.Drawing.Point(12, 12);
+            this.pictureBox1.Location = new System.Drawing.Point(16, 15);
+            this.pictureBox1.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox1.Name = "pictureBox1";
-            this.pictureBox1.Size = new System.Drawing.Size(173, 130);
+            this.pictureBox1.Size = new System.Drawing.Size(230, 160);
             this.pictureBox1.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox1.TabIndex = 1;
             this.pictureBox1.TabStop = false;
@@ -64,18 +77,20 @@
             // 
             this.label1.AutoSize = true;
             this.label1.Cursor = System.Windows.Forms.Cursors.Default;
-            this.label1.Location = new System.Drawing.Point(29, 145);
+            this.label1.Location = new System.Drawing.Point(39, 178);
+            this.label1.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
             this.label1.Name = "label1";
-            this.label1.Size = new System.Drawing.Size(127, 13);
+            this.label1.Size = new System.Drawing.Size(162, 16);
             this.label1.TabIndex = 2;
             this.label1.Text = "Исходное изображение";
             // 
             // pictureBox2
             // 
             this.pictureBox2.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox2.Location = new System.Drawing.Point(319, 12);
+            this.pictureBox2.Location = new System.Drawing.Point(301, 13);
+            this.pictureBox2.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox2.Name = "pictureBox2";
-            this.pictureBox2.Size = new System.Drawing.Size(160, 130);
+            this.pictureBox2.Size = new System.Drawing.Size(237, 162);
             this.pictureBox2.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox2.TabIndex = 3;
             this.pictureBox2.TabStop = false;
@@ -83,38 +98,136 @@
             // pictureBox3
             // 
             this.pictureBox3.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
-            this.pictureBox3.Location = new System.Drawing.Point(587, 12);
+            this.pictureBox3.Location = new System.Drawing.Point(588, 13);
+            this.pictureBox3.Margin = new System.Windows.Forms.Padding(4);
             this.pictureBox3.Name = "pictureBox3";
-            this.pictureBox3.Size = new System.Drawing.Size(160, 130);
+            this.pictureBox3.Size = new System.Drawing.Size(227, 162);
+            this.pictureBox3.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
             this.pictureBox3.TabIndex = 4;
             this.pictureBox3.TabStop = false;
             // 
             // button2
             // 
-            this.button2.Location = new System.Drawing.Point(41, 244);
+            this.button2.Location = new System.Drawing.Point(56, 318);
+            this.button2.Margin = new System.Windows.Forms.Padding(4);
             this.button2.Name = "button2";
-            this.button2.Size = new System.Drawing.Size(104, 48);
+            this.button2.Size = new System.Drawing.Size(139, 59);
             this.button2.TabIndex = 5;
             this.button2.Text = "Преобразовать";
             this.button2.UseVisualStyleBackColor = true;
             this.button2.Click += new System.EventHandler(this.button2_Click);
             // 
+            // pictureBox4
+            // 
+            this.pictureBox4.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox4.Location = new System.Drawing.Point(856, 13);
+            this.pictureBox4.Name = "pictureBox4";
+            this.pictureBox4.Size = new System.Drawing.Size(227, 162);
+            this.pictureBox4.SizeMode = System.Windows.Forms.PictureBoxSizeMode.StretchImage;
+            this.pictureBox4.TabIndex = 6;
+            this.pictureBox4.TabStop = false;
+            // 
+            // pictureBox5
+            // 
+            this.pictureBox5.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox5.Location = new System.Drawing.Point(301, 230);
+            this.pictureBox5.Name = "pictureBox5";
+            this.pictureBox5.Size = new System.Drawing.Size(237, 147);
+            this.pictureBox5.TabIndex = 7;
+            this.pictureBox5.TabStop = false;
+            // 
+            // pictureBox6
+            // 
+            this.pictureBox6.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox6.Location = new System.Drawing.Point(588, 230);
+            this.pictureBox6.Name = "pictureBox6";
+            this.pictureBox6.Size = new System.Drawing.Size(227, 147);
+            this.pictureBox6.TabIndex = 8;
+            this.pictureBox6.TabStop = false;
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label2.Location = new System.Drawing.Point(352, 178);
+            this.label2.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(138, 16);
+            this.label2.TabIndex = 9;
+            this.label2.Text = "Оттенки серого PAL";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label3.Location = new System.Drawing.Point(627, 178);
+            this.label3.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(151, 16);
+            this.label3.TabIndex = 10;
+            this.label3.Text = "Оттенки серого HDTV";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label4.Location = new System.Drawing.Point(939, 178);
+            this.label4.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(64, 16);
+            this.label4.TabIndex = 11;
+            this.label4.Text = "Разница";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label5.Location = new System.Drawing.Point(352, 380);
+            this.label5.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(120, 16);
+            this.label5.TabIndex = 12;
+            this.label5.Text = "Гистограмма PAL";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Cursor = System.Windows.Forms.Cursors.Default;
+            this.label6.Location = new System.Drawing.Point(639, 380);
+            this.label6.Margin = new System.Windows.Forms.Padding(4, 0, 4, 0);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(133, 16);
+            this.label6.TabIndex = 13;
+            this.label6.Text = "Гистограмма HDTV";
+            // 
             // Form1
             // 
-            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
-            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.ClientSize = new System.Drawing.Size(1101, 417);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.pictureBox6);
+            this.Controls.Add(this.pictureBox5);
+            this.Controls.Add(this.pictureBox4);
             this.Controls.Add(this.button2);
             this.Controls.Add(this.pictureBox3);
             this.Controls.Add(this.pictureBox2);
             this.Controls.Add(this.label1);
             this.Controls.Add(this.pictureBox1);
             this.Controls.Add(this.button1);
+            this.Margin = new System.Windows.Forms.Padding(4);
             this.Name = "Form1";
-            this.Text = "Form1";
+            this.Text = "Оттенки серого";
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox2)).EndInit();
             ((System.ComponentModel.ISupportInitialize)(this.pictureBox3)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox4)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox5)).EndInit();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox6)).EndInit();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -129,6 +242,14 @@
         private System.Windows.Forms.PictureBox pictureBox2;
         private System.Windows.Forms.PictureBox pictureBox3;
         private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.PictureBox pictureBox4;
+        private System.Windows.Forms.PictureBox pictureBox5;
+        private System.Windows.Forms.PictureBox pictureBox6;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
     }
 }
 
--- lab2/task1/lesson2_task1/lesson2_task1/Form1.cs
+++ lab2/task1/lesson2_task1/lesson2_task1/Form1.cs
@@ -29,12 +29,79 @@ namespace lesson2_task1
                 {
                     Color originalColor = img.GetPixel(i, j);
                     Color newColor = Color.Black;
-                    newColor = Color.FromArgb(originalColor.R, originalColor.G, originalColor.B);
+                    int grayValue = (int)(0.299 * originalColor.R + 0.587 * originalColor.G + 0.114 * originalColor.B);
+                    newColor = Color.FromArgb(grayValue, grayValue, grayValue);
                     newImage.SetPixel(i, j, newColor);
                 }
             }
 
             return newImage;
+        }
+
+        private static Bitmap ToGrayHDTV(Bitmap img)
+        {
+            int width = img.Width;
+            int height = img.Height;
+            Bitmap newImage = new Bitmap(width, height);
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    Color originalColor = img.GetPixel(i, j);
+                    Color newColor = Color.Black;
+                    int grayValue = (int)(0.2126 * originalColor.R + 0.7152 * originalColor.G + 0.0722 * originalColor.B);
+                    newColor = Color.FromArgb(grayValue, grayValue, grayValue);
+                    newImage.SetPixel(i, j, newColor);
+                }
+            }
+            return newImage;
+        }
+
+        private static Bitmap ImagesDifference(Bitmap img1, Bitmap img2)
+        {
+            int width = img1.Width;
+            int height = img1.Height;
+            Bitmap diffImage = new Bitmap(img1.Width, img1.Height);
+
+            for (int i = 0; i < width; i++)
+            {
+                for (int j = 0; j < height; j++)
+                {
+                    Color color1 = img1.GetPixel(i, j);
+                    Color color2 = img2.GetPixel(i, j);
+                    int diffR = Math.Abs(color1.R - color2.R);
+                    int diffG = Math.Abs(color1.G - color2.G);
+                    int diffB = Math.Abs(color1.B - color2.B);
+                    diffImage.SetPixel(i, j, Color.FromArgb(diffR, diffG, diffB));
+                }
+            }
+            return diffImage;
+        }
+        private static Bitmap Histogram(Bitmap img)
+        {
+            Bitmap histogramImage = new Bitmap(256, 256);
+
+            int histogramHeight = histogramImage.Height;
+            int[] intensity = new int[256];
+            for (int i = 0; i < img.Width; i++)
+            {
+                for (int j = 0; j < img.Height; j++)
+                {
+                    Color color = img.GetPixel(i, j);
+                    intensity[color.R]++;
+                }
+            }
+
+            Graphics g = Graphics.FromImage(histogramImage);
+            g.Clear(Color.White);
+            Pen pen = new Pen(Color.Black);
+            for (int i = 0; i < 256; i++)
+            {
+                g.DrawLine(pen, i, histogramImage.Height, i, histogramImage.Height - intensity[i]);
+            }
+
+            return histogramImage;
         }
 
         private void button1_Click(object sender, EventArgs e)
@@ -62,9 +129,19 @@ namespace lesson2_task1
             Bitmap originalImage = new Bitmap(pictureBox1.Image);
 
             Bitmap imagePal = ToGrayPal(originalImage);
+            Bitmap imageHDTV = ToGrayHDTV(originalImage);
 
             pictureBox2.Image = imagePal;
+            pictureBox3.Image = imageHDTV;
 
+            Bitmap imageRz = ImagesDifference(imagePal, imageHDTV);
+
+            pictureBox4.Image = imageRz;
+
+            Bitmap HistogramPal = Histogram(imagePal);
+            pictureBox5.Image = HistogramPal;
+            Bitmap HistogramHDTV = Histogram(imageHDTV);
+            pictureBox6.Image = HistogramHDTV;
         }
     }
 }
