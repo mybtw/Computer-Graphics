0f46d88d43d30f19aaf8a11ef63f47f7b159b070
artx <35874101+artx52135@users.noreply.github.com>
2023-10-16 18:49:44 +0300

    Add lab5 task3

---
+++ lab5/task3/BezierCurve/Form1.Designer.cs
@@ -0,0 +1,138 @@
+﻿namespace BezierCurve
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.button1 = new System.Windows.Forms.Button();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(3, 35);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(939, 525);
+            this.pictureBox1.TabIndex = 0;
+            this.pictureBox1.TabStop = false;
+            this.pictureBox1.MouseClick += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseClick);
+            this.pictureBox1.MouseDown += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseDown);
+            this.pictureBox1.MouseUp += new System.Windows.Forms.MouseEventHandler(this.pictureBox1_MouseUp);
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton1.Location = new System.Drawing.Point(307, 9);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(88, 20);
+            this.radioButton1.TabIndex = 1;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "Добавить";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton2.Location = new System.Drawing.Point(408, 9);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(80, 20);
+            this.radioButton2.TabIndex = 1;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Удалить";
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.radioButton3.Location = new System.Drawing.Point(494, 9);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(112, 20);
+            this.radioButton3.TabIndex = 1;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Переместить";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Microsoft Sans Serif", 9.75F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.label1.Location = new System.Drawing.Point(12, 13);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(206, 16);
+            this.label1.TabIndex = 2;
+            this.label1.Text = "Действия с опорными точками";
+            // 
+            // button1
+            // 
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 11.25F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.button1.Location = new System.Drawing.Point(680, 1);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(168, 28);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Очистить";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(946, 560);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.pictureBox1);
+            this.Name = "Form1";
+            this.Text = "Кубические сплайны Безье";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Button button1;
+    }
+}
+
---
+++ lab5/task3/BezierCurve/Form1.cs
@@ -0,0 +1,207 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace BezierCurve
+{
+    public partial class Form1 : Form
+    {
+        private List<PointF> points;
+        private Bitmap bmp;
+
+        private float[,] BezierMatrix = { {  1,  -3, 3,  -1 },  // Матрица Безье
+                                          {  0,  3,  -6,  3 },
+                                          {  0,  0,  3,  -3 },
+                                          {  0,  0,  0,  1 }};
+
+        private PointF newPoint;
+        private int index_point; // индекс точки, которую будем передвигать
+
+        //перемножение матриц
+        private float[,] multMatrix(float[,] m1, float[,] m2)
+        {
+            float[,] res = new float[m1.GetLength(0), m2.GetLength(1)];
+
+            for (int i = 0; i < m1.GetLength(0); ++i)
+                for (int j = 0; j < m2.GetLength(1); ++j)
+                    for (int k = 0; k < m2.GetLength(0); k++)
+                    {
+                        res[i, j] += m1[i, k] * m2[k, j];
+                    }
+
+            return res;
+        }
+        public Form1()
+        {
+            InitializeComponent();
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = bmp;
+            pictureBox1.BackColor = Color.White;
+            points = new List<PointF>();
+            radioButton1.Checked = true;
+            index_point = -1;
+            newPoint = new Point();
+        }
+        // Очистка 
+        private void button2_Click(object sender, EventArgs e)
+        {
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = bmp;
+            points = new List<PointF>();
+            radioButton1.Checked = true;
+            index_point = -1;
+            newPoint = new PointF();
+            pictureBox1.BackColor = Color.White;
+        }
+
+        private void pictureBox1_MouseClick(object sender, MouseEventArgs e)
+        {
+            if (radioButton1.Checked) // если выбрано "добавление точки"
+            {
+                points.Add(e.Location);
+            }
+            if (radioButton2.Checked) // если выбрано "удаление точки"
+            {
+                DeletePoint(e.Location.X, e.Location.Y);
+            }
+            DrawElements();
+        }
+        private void pictureBox1_MouseDown(object sender, MouseEventArgs e)
+        {
+            if (radioButton3.Checked)
+            {
+                index_point = points.FindIndex(el => (el.X > e.X - 5) && (el.X < e.X + 5) && (el.Y > e.Y - 5) && (el.Y < e.Y + 5));
+            }
+        }
+        private void pictureBox1_MouseUp(object sender, MouseEventArgs e)
+        {
+            if (radioButton3.Checked)
+            {
+                if (index_point >= 0)
+                {
+                    points[index_point] = new PointF(e.X, e.Y);
+                    DeletePoint(newPoint.X, newPoint.Y);
+                    DrawElements();
+                    index_point = -1;
+                }
+            }
+        }
+        // Удаление точки
+        private void DeletePoint(float x, float y)
+        {
+            int indexDel = points.FindIndex(el => (el.X > x - 5) && (el.X < x + 5) && (el.Y > y - 5) && (el.Y < y + 5));
+            if (indexDel >= 0)
+            {
+                points.RemoveAt(indexDel);
+                DrawElements();
+            }
+        }
+
+        private void DrawElements()
+        {
+            bmp = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            pictureBox1.Image = bmp;
+            DrawPoints();
+            DrawCurve();
+            
+        }
+
+        // Рисуем опорные точки
+        private void DrawPoints()
+        {
+            SolidBrush solidBrush = new SolidBrush(Color.Red);
+            Graphics g = Graphics.FromImage(bmp);
+            Pen p = new Pen(Color.Red);
+            foreach (var point in points)
+            {
+                if (point != newPoint)
+                {
+                    g.DrawEllipse(p, point.X - 3, point.Y - 3, 6, 6);
+                    g.FillEllipse(solidBrush, point.X - 3, point.Y - 3, 6, 6);
+                }
+            }
+            pictureBox1.Image = bmp;
+        }
+        
+        // Функция для отрисовки каждой точки кривой
+        private PointF GetNextPointOfCurve(PointF p0, PointF p1, PointF p2, PointF p3, float t)
+        {
+            float[,] MatrPointsX = { { p0.X, p1.X, p2.X, p3.X } };
+            float[,] MatrPointsY = { { p0.Y, p1.Y, p2.Y, p3.Y } };
+
+            float[,] MatrParametrs = { { 1 }, { t }, { t * t }, {t*t*t } };
+
+            
+            float X = multMatrix(multMatrix(MatrPointsX,BezierMatrix),MatrParametrs)[0, 0];
+            float Y = multMatrix(multMatrix(MatrPointsY, BezierMatrix), MatrParametrs)[0, 0];
+
+            return new PointF(X, Y);
+        }
+
+        // Нарисовать кривую по 4 опорным точкам
+        private void DrawCurveFor4Points(PointF p0, PointF p1, PointF p2, PointF p3)
+        {
+            int segments = 100; // Количество сегментов для отрисовки кривой
+            PointF prevPixel = GetNextPointOfCurve(p0, p1, p2, p3, 0);
+
+            using (Graphics g = Graphics.FromImage(bmp))
+            using (Pen pen = new Pen(Color.Black))
+            {
+                for (int i = 1; i <= segments; i++)
+                {
+                    float t = i / (float)segments;
+                    var pixel = GetNextPointOfCurve(p0, p1, p2, p3, t);
+
+                    // Рисуем линию между предыдущей и текущей точкой
+                    g.DrawLine(pen, prevPixel, pixel);
+
+                    prevPixel = pixel;
+                }
+            }
+        }
+
+        // Получение дополнительных точек
+        private PointF GetExtraPoint(PointF point1, PointF point2)
+        {
+            return new PointF((point1.X + point2.X) / 2, (point1.Y + point2.Y) / 2);
+        }
+
+
+        private void DrawCurveFor3Points(PointF p0, PointF p1, PointF p2)
+        {
+            PointF p12 = GetExtraPoint(p1, p2);
+            DrawCurveFor4Points(p0, p1, p12, p2);
+
+        }
+        // Функция для отрисовки кривой по множеству точек
+        private void DrawCurve()
+        {
+            PointF prev, next;
+            prev = points[0];
+            int points_size = points.Count();
+            if (points_size == 4)
+            {
+                DrawCurveFor4Points(points[0], points[1], points[2], points[3]);
+            }
+            else if (points_size > 4)
+            {
+                for (int i = 0; i < points.Count - 4; i += 2)
+                {
+                    next = new PointF((points[i + 2].X + points[i + 3].X) / 2, (points[i + 2].Y + points[i + 3].Y) / 2);
+                    DrawCurveFor4Points(prev, points[i + 1], points[i + 2], next);
+                    prev = next;
+                }
+                if (points.Count % 2 == 0)
+                    DrawCurveFor4Points(prev, points[points.Count - 3], points[points.Count - 2], points[points.Count - 1]);
+                else
+                    DrawCurveFor3Points(prev, points[points.Count - 2], points[points.Count - 1]);
+            }
+        }
+    }
+}
