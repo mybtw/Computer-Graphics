cc0c06db7e7ea2d0b5fe8fe45f673f405955bd74
Moirted <moirted@gmail.com>
2023-10-21 15:34:05 +0300

    Midpoint Realized

---
+++ lab5/task2/midpoint_displacement/midpoint_displacement/Form1.Designer.cs
@@ -0,0 +1,226 @@
+﻿namespace midpoint_displacement
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Обязательная переменная конструктора.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Освободить все используемые ресурсы.
+        /// </summary>
+        /// <param name="disposing">истинно, если управляемый ресурс должен быть удален; иначе ложно.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Код, автоматически созданный конструктором форм Windows
+
+        /// <summary>
+        /// Требуемый метод для поддержки конструктора — не изменяйте 
+        /// содержимое этого метода с помощью редактора кода.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.button1 = new System.Windows.Forms.Button();
+            this.pictureBox1 = new System.Windows.Forms.PictureBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.checkBox1 = new System.Windows.Forms.CheckBox();
+            this.label2 = new System.Windows.Forms.Label();
+            this.label3 = new System.Windows.Forms.Label();
+            this.label4 = new System.Windows.Forms.Label();
+            this.label5 = new System.Windows.Forms.Label();
+            this.label6 = new System.Windows.Forms.Label();
+            this.textBox4 = new System.Windows.Forms.TextBox();
+            this.label7 = new System.Windows.Forms.Label();
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).BeginInit();
+            this.SuspendLayout();
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(24, 12);
+            this.textBox1.MaxLength = 10;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(100, 22);
+            this.textBox1.TabIndex = 0;
+            this.textBox1.Text = "7";
+            this.textBox1.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(161, 12);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(100, 22);
+            this.textBox2.TabIndex = 1;
+            this.textBox2.Text = "20";
+            this.textBox2.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // textBox3
+            // 
+            this.textBox3.Location = new System.Drawing.Point(297, 12);
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(100, 22);
+            this.textBox3.TabIndex = 2;
+            this.textBox3.Text = "200";
+            this.textBox3.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(544, 12);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(171, 39);
+            this.button1.TabIndex = 3;
+            this.button1.Text = "Генерировать";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // pictureBox1
+            // 
+            this.pictureBox1.BorderStyle = System.Windows.Forms.BorderStyle.FixedSingle;
+            this.pictureBox1.Location = new System.Drawing.Point(24, 64);
+            this.pictureBox1.Name = "pictureBox1";
+            this.pictureBox1.Size = new System.Drawing.Size(800, 400);
+            this.pictureBox1.TabIndex = 4;
+            this.pictureBox1.TabStop = false;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(40, 37);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(62, 16);
+            this.label1.TabIndex = 5;
+            this.label1.Text = "Глубина";
+            // 
+            // checkBox1
+            // 
+            this.checkBox1.AutoSize = true;
+            this.checkBox1.CheckAlign = System.Drawing.ContentAlignment.BottomCenter;
+            this.checkBox1.Location = new System.Drawing.Point(412, 12);
+            this.checkBox1.Name = "checkBox1";
+            this.checkBox1.Size = new System.Drawing.Size(99, 37);
+            this.checkBox1.TabIndex = 6;
+            this.checkBox1.Text = "Сглаживание";
+            this.checkBox1.TextAlign = System.Drawing.ContentAlignment.TopCenter;
+            this.checkBox1.UseVisualStyleBackColor = true;
+            this.checkBox1.CheckedChanged += new System.EventHandler(this.checkBox1_CheckedChanged);
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(158, 37);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(107, 16);
+            this.label2.TabIndex = 7;
+            this.label2.Text = "Разница Высот";
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(830, 257);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(28, 16);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "200";
+            // 
+            // label4
+            // 
+            this.label4.AutoSize = true;
+            this.label4.Location = new System.Drawing.Point(839, 448);
+            this.label4.Name = "label4";
+            this.label4.Size = new System.Drawing.Size(14, 16);
+            this.label4.TabIndex = 9;
+            this.label4.Text = "0";
+            // 
+            // label5
+            // 
+            this.label5.AutoSize = true;
+            this.label5.Location = new System.Drawing.Point(830, 64);
+            this.label5.Name = "label5";
+            this.label5.Size = new System.Drawing.Size(28, 16);
+            this.label5.TabIndex = 10;
+            this.label5.Text = "400";
+            // 
+            // label6
+            // 
+            this.label6.AutoSize = true;
+            this.label6.Location = new System.Drawing.Point(318, 37);
+            this.label6.Name = "label6";
+            this.label6.Size = new System.Drawing.Size(55, 16);
+            this.label6.TabIndex = 11;
+            this.label6.Text = "Высота";
+            // 
+            // textBox4
+            // 
+            this.textBox4.Location = new System.Drawing.Point(746, 12);
+            this.textBox4.Name = "textBox4";
+            this.textBox4.Size = new System.Drawing.Size(69, 22);
+            this.textBox4.TabIndex = 12;
+            this.textBox4.Text = "300";
+            this.textBox4.TextAlign = System.Windows.Forms.HorizontalAlignment.Center;
+            // 
+            // label7
+            // 
+            this.label7.AutoSize = true;
+            this.label7.Location = new System.Drawing.Point(743, 37);
+            this.label7.Name = "label7";
+            this.label7.Size = new System.Drawing.Size(72, 16);
+            this.label7.TabIndex = 13;
+            this.label7.Text = "Задержка";
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(868, 479);
+            this.Controls.Add(this.label7);
+            this.Controls.Add(this.textBox4);
+            this.Controls.Add(this.label6);
+            this.Controls.Add(this.label5);
+            this.Controls.Add(this.label4);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.checkBox1);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.pictureBox1);
+            this.Controls.Add(this.button1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.FormBorderStyle = System.Windows.Forms.FormBorderStyle.FixedSingle;
+            this.Name = "Form1";
+            this.Text = "Генератор гор";
+            ((System.ComponentModel.ISupportInitialize)(this.pictureBox1)).EndInit();
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.PictureBox pictureBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.CheckBox checkBox1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.Label label4;
+        private System.Windows.Forms.Label label5;
+        private System.Windows.Forms.Label label6;
+        private System.Windows.Forms.TextBox textBox4;
+        private System.Windows.Forms.Label label7;
+    }
+}
+
---
+++ lab5/task2/midpoint_displacement/midpoint_displacement/Form1.cs
@@ -0,0 +1,128 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace midpoint_displacement
+{
+   
+
+    public partial class Form1 : Form
+    {
+        private MountainGenerator mountainGenerator;
+        private Bitmap mountainBitmap;
+        private int depth; // Глубина рекурсии
+        private int displacementRange; // Величина смещения
+        private int initialHeight; // Начальные высоты
+        private bool smoothing; // Флаг сглаживания
+        int delayMilliseconds = 300; // Время задержки между шагами (в миллисекундах)
+        public Form1()
+
+        {
+            InitializeComponent();
+            depth = 7;
+            displacementRange = 20;
+            initialHeight = pictureBox1.Height / 2;
+            smoothing = false;
+            mountainGenerator = new MountainGenerator();
+            mountainBitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+            GenerateMountainAsync();
+        }
+
+        private async Task GenerateMountainAsync()
+        {
+            await Task.Run(async () =>
+            { 
+                mountainBitmap = new Bitmap(pictureBox1.Width, pictureBox1.Height);
+                Graphics g = Graphics.FromImage(mountainBitmap);
+
+                int[] points = new int[pictureBox1.Width];
+                points[0] = initialHeight;
+                points[pictureBox1.Width - 1] = initialHeight;
+
+                for (int step = 0; step < depth; step++)
+                {
+                    mountainGenerator.GenerateMountainRecursive(points, 0, pictureBox1.Width - 1, step, displacementRange);
+
+                    // Отрисовка горного массива после каждого шага
+                    DrawMountain(g, points);
+                    pictureBox1.Image = mountainBitmap;
+
+                    await Task.Delay(delayMilliseconds);
+                    if (smoothing)
+                    {
+                        mountainGenerator.Smooth(points, 18);
+                    }
+                }
+            });
+        }
+
+        private void DrawMountain(Graphics g, int[] points)
+        {
+            g.Clear(Color.White);
+            Pen pen = new Pen(Color.Black);
+            for (int x = 0; x < points.Length - 1; x++)
+            {
+                g.DrawLine(pen, x, pictureBox1.Height - points[x], x + 1, pictureBox1.Height - points[x + 1]);
+            }
+        }
+
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            Int32.TryParse(textBox1.Text, out depth);
+            Int32.TryParse(textBox2.Text, out displacementRange);
+            Int32.TryParse(textBox3.Text, out initialHeight);
+            Int32.TryParse(textBox4.Text, out delayMilliseconds);
+            GenerateMountainAsync();
+        }
+
+        private void checkBox1_CheckedChanged(object sender, EventArgs e)
+        {
+            smoothing = checkBox1.Checked;
+        }
+    }
+
+    public class MountainGenerator
+    {
+        private Random random = new Random();  
+
+        public void GenerateMountainRecursive(int[] points, int left, int right, int depth, int displacementRange)
+        {
+            if (depth == 0)
+                return;
+
+            int middle = (left + right) / 2;
+            int displacement = (points[left] + points[right]) / 2 + random.Next(-displacementRange, displacementRange);
+
+            points[middle] = displacement;
+
+            GenerateMountainRecursive(points, left, middle, depth - 1, displacementRange);
+            GenerateMountainRecursive(points, middle, right, depth - 1, displacementRange);
+        }
+
+        public void Smooth(int[] points, int passes)
+        {
+            int[] smoothedPoints = new int[points.Length];
+
+            for (int i = 0; i < passes; i++)
+            {
+                for (int x = 1; x < points.Length - 1; x++)
+                {
+                    smoothedPoints[x] = (points[x - 1] + points[x] + points[x + 1]) / 3;
+                }
+
+                for (int x = 1; x < points.Length - 1; x++)
+                {
+                    points[x] = smoothedPoints[x];
+                }
+            }
+        }
+    }
+
+}
